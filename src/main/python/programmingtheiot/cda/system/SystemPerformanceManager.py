3de0c0eac5373b90d9963e2362d26abbc2379bc3
ZuZunja <taylor.jonat@northeastern.edu>
2024-10-21 17:00:32 -0400

    MergingCDALabModule5

--- src/main/python/programmingtheiot/cda/system/SystemPerformanceManager.py
+++ src/main/python/programmingtheiot/cda/system/SystemPerformanceManager.py
@@ -56,9 +56,17 @@ class SystemPerformanceManager(object):
 		memUtilPct = self.memUtilTask.getTelemetryValue()
 	
 		logging.debug('CPU utilization is %s percent, and memory utilization is %s percent.', str(cpuUtilPct), str(memUtilPct))
+		sysPerfData = SystemPerformanceData()
+		sysPerfData.setLocationID(self.locationID)
+		sysPerfData.setCpuUtilization(self.cpuUtilPct)
+		sysPerfData.setMemoryUtilization(self.memUtilPct)
+	
+		if self.dataMsgListener:
+			self.dataMsgListener.handleSystemPerformanceMessage(data = sysPerfData)
 		
 	def setDataMessageListener(self, listener: IDataMessageListener) -> bool:
-		pass
+		if listener:
+			self.dataMsgListener = listener
 	
 	def startManager(self):
 		logging.info("Starting SystemPerformanceManager...")
